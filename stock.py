6bda416083b304065156d2020a2cd71c22f56ce4
Akira <akira_yihun@qq.com>
2022-05-22 15:36:38 +0800

    commit from code first

---
+++ stock.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+
+def bondprice(cr, r, c, t):
+    tlis = list(range(1,t+1))
+    def discount(cr, r, c, n):
+        cashlis = [(c*r)/(1+cr)**a for a in range(1,n+1)]
+        cashlis.append(c/(1+cr)**n)
+        return sum(cashlis)
+    ylis = [discount(cr, r, c, i) for i in tlis]
+    plt.plot(tlis,ylis)
+    plt.show()
+
+bondprice(0.15,0.03,100,30)
